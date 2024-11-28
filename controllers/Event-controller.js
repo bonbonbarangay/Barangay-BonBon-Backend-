dd966a9ee9e6e5d2e21f0436d8517d1ec38da8cd
radz22 <radzsantillan01@gmail.com>
2024-11-28 16:20:03 +0800

    backend event controller

--- controllers/Event-controller.js
+++ controllers/Event-controller.js
@@ -1,25 +1,38 @@
 import pool from "../dbconfig/database-setup.js";
+import cloudinary from "../services/cloudinary.js";
 
 export const createEvent = async (request, response) => {
   try {
-    const { title, date } = request.body;
-    console.log(title, date);
+    const { title, date, location, description, image } = request.body;
+    const uploadedResponse = await cloudinary.v2.uploader.upload(image, {
+      upload_preset: "",
+    });
 
+    if (!uploadedResponse) {
+      return response.status(400).json({
+        message: "cloudinary error",
+      });
+    }
     const createEvent = await pool.query(
-      "INSERT INTO public.event (title, date) VALUES ($1, $2) RETURNING *",
-      [title, date]
+      "INSERT INTO public.event (title, date, location, description, image, cloudinaryid ) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [
+        title,
+        date,
+        location,
+        description,
+        uploadedResponse.url,
+        uploadedResponse.public_id,
+      ]
     );
 
-    if (createEvent.rowCount === 0) {
-      return response.status(400).json({ message: "Failed to create event" });
+    if (!createEvent) {
+      return response.status(400).json({ message: "error create" });
     }
-
     return response.status(201).json({
-      message: "Created successfully",
+      message: "Event created successfully",
       event: createEvent.rows[0],
     });
   } catch (error) {
-    // Handle any errors
     return response.status(500).json({
       message: "Internal server error",
       error: error.message,
@@ -41,16 +54,47 @@ export const getAllEvent = async (request, response) => {
 
 export const updateEvent = async (request, response) => {
   try {
-    const { title, date } = request.body;
+    const { title, date, location, description, image, cloudinaryid } =
+      request.body;
     const { id } = request.params;
 
-    const updateEvent = await pool.query(
-      "UPDATE public.event SET title = $1, date = $2 WHERE id = $3 RETURNING *;",
-      [title, date, id]
-    );
+    let imageUrl;
+    let cloudinaryId;
 
+    const eventResult = await pool.query(
+      "SELECT image, cloudinaryid FROM public.event WHERE id = $1",
+      [id]
+    );
+    if (eventResult.rowCount === 0) {
+      return response.status(404).json({
+        message: "Official not found",
+      });
+    }
+
+    if (eventResult.rows[0].image == image) {
+      imageUrl = eventResult.rows[0].image;
+      cloudinaryId = eventResult.rows[0].cloudinaryid;
+    } else {
+      await cloudinary.v2.uploader.destroy(cloudinaryid);
+      const uploadImageUpdate = await cloudinary.v2.uploader.upload(image, {
+        upload_preset: "",
+      });
+
+      if (!uploadImageUpdate) {
+        return response.status(400).json({
+          message: "Cloudinary upload error",
+        });
+      }
+      imageUrl = uploadImageUpdate.url;
+      cloudinaryId = uploadImageUpdate.public_id;
+    }
+
+    const updateEvent = await pool.query(
+      "UPDATE public.event SET title = $1,  date = $2 , location = $3, description = $4, image = $5, cloudinaryid = $6  WHERE id = $7 RETURNING *;",
+      [title, date, location, description, imageUrl, cloudinaryId, id]
+    );
     if (updateEvent.rowCount === 0) {
-      return response.status(400).json({
+      return response.status(404).json({
         message: "Official not found",
       });
     }
